ed1c1f13251a42da4134fca14e734922f010bb42
brianestimastersllc-coder <14pwciv4132@uetpeshawar.edu.pk>
2026-07-04 16:20:59 +0500

    Add Loans tab: borrow/lend tracking with due dates, payments, overdue alerts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kharcha-v1';
+const CACHE = 'kharcha-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
