c356cad24fc5e963625bcf0b18f40fa078d45c4d
brianestimastersllc-coder <14pwciv4132@uetpeshawar.edu.pk>
2026-07-06 23:44:51 +0500

    Add Wealth tab: accounts with auto-updating balances, transfers with custom rates, PIN lock, masked amounts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kharcha-v2';
+const CACHE = 'kharcha-v3';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
